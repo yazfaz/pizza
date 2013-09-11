be05c4bc006288c31398214852f7f89c3ddcdcf4
MakerSquare Student <students@themakersquare.com>
2013-09-11 14:21:27 -0500

    Add vegetarian test

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -14,5 +14,10 @@ describe Pizza::Topping do
 
       expect(topping.name).to eq('olives')
     end
+    it 'sets whether or not the topping is vegetarian' do
+      topping = Pizza::Topping.new 'bell peppers', vegetarian: true
+      expect(topping.vegetarian).to eq(true)
+    end
   end
 end
+
